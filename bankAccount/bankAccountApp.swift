fb18b5721501b56fa3586fa66af58cbb477908a7
alex_ezhovv <aezhov4@gmail.com>
2022-07-10 00:54:26 +0800

    Initial Commit

---
+++ bankAccount/bankAccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  bankAccountApp.swift
+//  bankAccount
+//
+//  Created by Артём Рябенко on 10.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct bankAccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
